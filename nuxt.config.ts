0c6b59661269ebb09bfd0f2af5665b0f635e8930
ponyoxa <kmsm805@gmail.com>
2024-11-10 22:00:56 +0900

    OGP 追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,23 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/png', href: '/favicon.png' },
         { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=BIZ+UDPGothic&display=swap" }
+      ],
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: http://ogp.me/ns#'
+      },
+      title: 'ponyoxa blog',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'ponyoxa が運営するブログです' },
+        { hid: 'og:site_name', property: 'og:site_name', content: 'ponyoxa blog' },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        { hid: 'og:url', property: 'og:url', content: 'https://ponyoxa.com/' },
+        { hid: 'og:title', property: 'og:title', content: 'ponyoxa blog' },
+        { hid: 'og:description', property: 'og:description', content: 'ponyoxa が運営するブログです' },
+        { hid: 'og:image', property: 'og:image', content: '/blog-ogp-2.png' },
+        { name: 'twitter:card', content: 'summary' }
       ]
     }
   }
