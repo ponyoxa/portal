aeaaa23d7b430f18342267a57a8498cfab218f17
ponyoxa <kmsm805@gmail.com>
2024-12-20 19:44:36 +0900

    ファイル作成用シェルスクリプト

---
+++ report-file-generator.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 今日の日付を取得 (フォーマット: YYYY-MM-DD)
+today=$(date +%Y-%m-%d)
+
+filename="${today}.md"
+
+# 出力先ディレクトリのパスを作成
+output_dir="./content/reports"
+
+# ファイルパスの設定
+file_path="${output_dir}/${filename}"
+
+# メタデータ付きのファイル内容を作成
+cat <<EOF > "$file_path"
+---
+title: 
+description: 
+createdAt: $today
+updatedAt: $today
+---
+EOF
+
+echo "File created at: $file_path"
