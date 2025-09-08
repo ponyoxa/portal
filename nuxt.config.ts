3f0f0cc2afec10fdd7b49f45c7df8905fb741dbd
ponyoxa <kmsm805@gmail.com>
2025-09-08 22:08:43 +0900

    add config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,34 @@
 import { defineNuxtConfig } from "nuxt/config";
 
+import fg from "fast-glob";
+
+const mdToRoutes = (patterns: string[]) =>
+  fg
+    .sync(patterns, { dot: false })
+    .map(
+      (p) =>
+        "/" +
+        p
+          .replace(/^content\//, "") // content配下をURLに
+          .replace(/\.md$/, "") // 拡張子除去
+    )
+    .map((r) => (r.endsWith("/index") ? r.slice(0, -6) : r)); // /xxx/index -> /xxx
+
+const blogRoutes = mdToRoutes(["content/blog/**/*.md"]);
+const diaryRoutes = mdToRoutes(["content/diaries/**/*.md"]);
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/content"],
+  experimental: {
+    renderJsonPayloads: true,
+  },
 
   routeRules: {
     "/": { prerender: true },
-    '/blog': { prerender: true },
+    "/blog": { prerender: true },
     "/blog/**": { prerender: true },
-    '/diaries': { prerender: true },
+    "/diaries": { prerender: true },
     "/diaries/**": { prerender: true },
   },
 
@@ -20,6 +40,10 @@ export default defineNuxtConfig({
   nitro: {
     preset: "cloudflare_module",
     cloudflare: {},
+    prerender: {
+      routes: ["/blog", "/diaries", ...blogRoutes, ...diaryRoutes],
+      crawlLinks: true,
+    },
   },
 
   app: {
