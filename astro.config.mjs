aa7b85dda52fe36d4041c9ca352aed7ece0ac294
ponyoxa <kmsm805@gmail.com>
2025-11-17 20:59:12 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ponyoxa.com',
-  output: 'static', // SSG モード
+  site: "https://ponyoxa.com",
+  output: "static", // SSG モード
   // adapter: cloudflare(), // static の場合は不要
 
   build: {
-    format: 'directory', // URL を /blog/post/ の形式に
+    format: "directory", // URL を /blog/post/ の形式に
   },
 });
