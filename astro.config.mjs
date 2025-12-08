c274573a6f5400f05b59be9ae5fd7df8a549bc0a
ponyoxa <kmsm805@gmail.com>
2025-12-08 22:46:49 +0900

    GA を導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import ogpGenerator from "./integrations/ogp-generator/index.js";
 
 // https://astro.build/config
@@ -12,5 +13,13 @@ export default defineConfig({
     format: "directory", // URL を /blog/post/ の形式に
   },
 
-  integrations: [mdx(), ogpGenerator()],
+  integrations: [
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    ogpGenerator(),
+  ],
 });
